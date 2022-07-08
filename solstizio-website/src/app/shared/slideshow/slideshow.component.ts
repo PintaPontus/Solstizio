0738d6ba15028ca0687e70394884bcf78eb55d1d
Blasco 001 <matteo.blascoviz@gmail.com>
2022-07-08 19:52:59 +0200

    fixes slideshow.component

--- solstizio-website/src/app/shared/slideshow/slideshow.component.ts
+++ solstizio-website/src/app/shared/slideshow/slideshow.component.ts
@@ -1,31 +1,49 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-slideshow',
-  templateUrl: './slideshow.component.html',
-  styleUrls: ['./slideshow.component.scss']
+    selector: 'app-slideshow',
+    templateUrl: './slideshow.component.html',
+    styleUrls: ['./slideshow.component.scss']
 })
 
 export class SlideshowComponent implements OnInit {
-  @Input() slides: Slide[] = [];
-  @Input() delay: number = 1000;
-  slideIndex: number = 0;
+    @Input() slides: Slide[] = [];
+    @Input() delay: number = 1000;
+    slideIndex: number = 0;
+    private clock: NodeJS.Timer | undefined;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-    this.startCounter();
-  }
+    ngOnInit(): void {
+        this.startCounter();
+    }
 
-  startCounter(){
-    setInterval(() => {
-      this.slideIndex = (this.slideIndex + 1) % this.slides.length;
-    }, this.delay);
-  }
+    startCounter() {
+        this.clock = setInterval(() => {
+            this.slideIndex = (this.slideIndex + 1) % this.slides.length;
+        }, this.delay);
+    }
+
+    nextSlide() {
+        clearInterval(this.clock);
+        this.clock = setInterval(() => {
+            this.slideIndex = (this.slideIndex + 1) % this.slides.length;
+        }, this.delay);
+        this.slideIndex = (this.slideIndex + 1) % this.slides.length;
+    }
+
+    previousSlide() {
+        clearInterval(this.clock);
+        this.clock = setInterval(() => {
+            this.slideIndex = (this.slideIndex + 1) % this.slides.length;
+        }, this.delay);
+        this.slideIndex = (this.slideIndex + this.slides.length - 1) % this.slides.length;
+    }
 
 }
 
 export interface Slide {
-  title: string,
-  backgroundUrl: string,
+    title: string,
+    backgroundUrl: string,
 }
